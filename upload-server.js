2ec2e4beca7e47d58a39c80c2136f25df10dd51e
rainyjune <rainyjune@live.cn>
2016-11-30 15:46:34 +0800

    Add upload-server.js

---
+++ upload-server.js
@@ -0,0 +1,23 @@
+const WebSocketServer = require('ws').Server,
+      wss = new WebSocketServer({port: 8181});
+
+const fs = require('fs');
+
+var chunks;
+
+wss.on('connection', (ws) => {
+  chunks = [];
+  console.log('client connected');
+  ws.on('message', (message) => {
+    console.log('message:', message);
+    
+    // When the Client sends a message 'EOF', it means the upload has been completed,  save chunks to a file.
+    if (message === "EOF") {
+      fs.writeFileSync('a.webm', Buffer.concat(chunks));
+      ws.send('DONE,total:' + chunks.length);
+    } else {
+      chunks.push(message);
+      ws.send(message);
+    }
+  });
+});
