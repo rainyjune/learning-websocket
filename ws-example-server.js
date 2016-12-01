36690cf8aae4b152c9de6fb15935182809406aa1
rainyjune <rainyjune@live.cn>
2016-12-01 10:56:01 +0800

    Update ws-example-server.js

--- ws-example-server.js
+++ ws-example-server.js
@@ -4,7 +4,6 @@ var WebSocketServer = require('ws').Server,
 wss.on('connection', function(ws) {
   ws.on('message', function(message) {
     console.log('received: %s', message);
-    ws.send(message);
   });
   console.log('client connected');
 });
